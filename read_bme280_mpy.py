ed24c60a448a04be75a450a06eb23af78cc9bb97
WjiM <wjim@viessmann.com>
2025-12-11 16:19:02 +0100

    tags standardized; refactor

---
+++ read_bme280_mpy.py
@@ -0,0 +1,29 @@
+from helpers import get_and_set_time
+
+def get_sensor_data_line_protocol(bme_sensor):
+    """Reads BME280 data and formats it into InfluxDB Line Protocol."""
+
+    try:
+        t, p, h = bme_sensor.read_compensated_data()
+        temp_c = round(t / 100, 2)
+        press_hpa = round(p / 256 / 100, 2)
+        hum_rh = round(h / 1024, 2)
+    except Exception as e:
+        print(f"Could not parse data strings: {e}")
+        return None
+
+    # The Measurement and Tag Set
+    measurement = "bme280_signals"
+    tags = "location=kitchen,sensor=bme280"
+
+    # The Field Set (using f-string formatting)
+    fields = f"temperature={temp_c},pressure={press_hpa},humidity={hum_rh}"
+
+    # Combine into Line Protocol
+    line_protocol = f"{measurement},{tags} {fields}"
+    print(
+        f"[{get_and_set_time()}] [RPi Pico] "
+        f"temperature: {temp_c} °C; pressure: {press_hpa} hPa; humidity: {hum_rh} %"
+    )
+
+    return line_protocol
--- write_bme280_signals.py
+++ write_bme280_signals.py
@@ -1,37 +1,36 @@
-import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+import influxdb_client, os
+from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import smbus2
 import bme280
-import time
 from datetime import datetime
 
 # Influx config
 token = os.environ.get("INFLUXDB_TOKEN")
 org = os.environ.get("INFLUXDB_ORG")
 url = os.environ.get("INFLUXDB_URL")
+bucket = os.environ.get("INFLUXDB_BUCKET")
 
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-bucket="temp_humidity_pressure"
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # reading signals from BME280 sensor
-address = 0x76
+ADDRESS = 0x76
 bus = smbus2.SMBus(1)
-calibration_params = bme280.load_calibration_params(bus, address)
+calibration_params = bme280.load_calibration_params(bus, ADDRESS)
+t, p, rh = bme280.read_compensated_data()
 
-data = bme280.sample(bus, address, calibration_params)
-t = round(data.temperature, 2)
-rh = round(data.humidity, 2)
-p = round(data.pressure, 2)
+temp_c = round(t / 100, 2)
+press_hpa = round(p / 256 / 100, 2)
+hum_rh = round(rh / 1024, 2)
 
 point = (
     Point("bme280_signals")
-    .tag("home", "iot")
+    .tag("location", "office")
     .field("temperature", t)
     .field("humidity", rh)
     .field("pressure", p)
 )
 
-write_api.write(bucket=bucket, org="home", record=point)
+write_api.write(bucket=bucket, org=org, record=point)
 print(f"[{datetime.now().isoformat()}] temp: {t}°C, humidity: {rh}%, pressure: {p}hPa")
